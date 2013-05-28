112f69b27e5d514e0fd0af8432bae1f1522fbea9
Francesco Dalprà <jabba.dev@gmail.com>
2013-05-28 23:55:06 +0000

    wip

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 var http = require('http'), url = require('url');
-var port = process.env.PORT||5000;
+//var port = process.env.PORT||8080;
+var port = 8080;
+console.log(process.env.PORT)
 http.createServer(function (req, res) {
-	var _url = url.parse(req.url);
 	
-	var reqOpt = {
-		host: _url.host, 
-		hostname: _url.hostname,
-		port: _url.port || 80,
-		localAddress: null,
-		socketPath: null,
-		method: req.method,
-		path: _url.path,
-		headers: req.headers,
-		auth: null,
-		agent: req.headers["user-agent"]
-	};
-
-	http.request(reqOpt);
-	
+	 var reqUrl = url.parse(req.url);
+	 console.log(reqUrl);
+	 var reqopt = {
+	 			host: reqUrl.host,
+	 			hostname: reqUrl.hostname,
+	 			port: req.port || 80,
+	 			localAddress: req.localAddress,
+	 			socketPath: req.socketPath,
+	 			method: req.method,
+	 			path: reqUrl.path,
+	 			auth: reqUrl.auth,
+	 			agent: req.agent,
+	 			headers: req.headers
+	 };
+	 
+	 console.log(reqopt);
+			
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Francesco hi!!! xxxx\n');
-  
+  res.end('Hello World new\n');
 }).listen(port,function(){
 		console.log('listen on ',port);
-});
-
-
-console.log('Server running at http://localhost:' + port );
+	});
+//}).listen(port,'localhost');
+console.log('Server xxx running at http://localhost:' + port );
