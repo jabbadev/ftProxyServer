cd82e00d582de0ce1f0634f675c4aa03ac2bc9a2
Francesco Dalprà <jabba.dev@gmail.com>
2013-05-29 10:31:01 +0200

    Test deploy

--- main.js
+++ main.js
@@ -1,30 +1,10 @@
-var http = require('http'), url = require('url');
-//var port = process.env.PORT||8080;
-var port = 8080;
-console.log(process.env.PORT)
+var http = require('http');
+var port = process.env.PORT||5000;
 http.createServer(function (req, res) {
-	
-	 var reqUrl = url.parse(req.url);
-	 console.log(reqUrl);
-	 var reqopt = {
-	 			host: reqUrl.host,
-	 			hostname: reqUrl.hostname,
-	 			port: req.port || 80,
-	 			localAddress: req.localAddress,
-	 			socketPath: req.socketPath,
-	 			method: req.method,
-	 			path: reqUrl.path,
-	 			auth: reqUrl.auth,
-	 			agent: req.agent,
-	 			headers: req.headers
-	 };
-	 
-	 console.log(reqopt);
-			
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World new\n');
+  res.end('Deploy OK\n');
 }).listen(port,function(){
 		console.log('listen on ',port);
 	});
 //}).listen(port,'localhost');
-console.log('Server xxx running at http://localhost:' + port );
+console.log('Server running at http://localhost:' + port );
